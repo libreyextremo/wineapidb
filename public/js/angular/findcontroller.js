24580771331c5dd739182d38ca36b014cf806109
JMSobrinos <libreyextremo@gmail.com>
2017-12-13 19:08:58 +0100

    bottom directive

--- public/js/angular/findcontroller.js
+++ public/js/angular/findcontroller.js
@@ -1,8 +1,8 @@
-angular
-	.module("findApp", [])
-	.controller("FindAppController", ["$scope", "$http", function($scope, $http) {
+var myApp = angular.module("findApp", []);
+myApp.controller("FindAppController", ["$scope", "$http", function($scope, $http) {
 		$scope.wines = [];
 		$scope.position = 0;
+		/*
 		$http.get("http://localhost:4000/api/wines")
 			.then(function(res) {
 				$scope.wines = res.data;
@@ -10,6 +10,12 @@ angular
 			}, function(res) {
 				$scope.wines = [{name: "Error!!" + res.status}];
 			});
-			
-	}]
-);
+			*/		
+		}]
+	);
+myApp.directive('bottom', function() {
+  return {
+    restrict: 'E',
+	templateUrl: 'bottom.html'
+  };
+});
